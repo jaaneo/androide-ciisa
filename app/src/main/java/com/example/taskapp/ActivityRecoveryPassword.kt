27885d3bcbfe2a9c00748ab9b6d341ae9e48388e
Alejandro Saez <jaaneo@gmail.com>
2022-05-30 18:19:58 -0400

    Agregado un mensaje al momento de presionar el botón recuperar contraseña

--- app/src/main/java/com/example/taskapp/ActivityRecoveryPassword.kt
+++ app/src/main/java/com/example/taskapp/ActivityRecoveryPassword.kt
@@ -1,11 +1,20 @@
 package com.example.taskapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class ActivityRecoveryPassword : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recovery_password)
+        val btnRecoveryPassword = findViewById<Button>(R.id.activity_recovery_password_btn_recovery)
+
+        btnRecoveryPassword.setOnClickListener {
+            Toast.makeText(this, "Revisa tu casilla de correo", Toast.LENGTH_LONG).show()
+        }
     }
 }
