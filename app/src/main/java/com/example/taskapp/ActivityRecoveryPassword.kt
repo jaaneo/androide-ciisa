be743c5ce9e629888b782a147ad3826e11c097b6
Alejandro Saez <jaaneo@gmail.com>
2022-05-30 18:15:32 -0400

    Agregada la vista recovery_password

---
+++ app/src/main/java/com/example/taskapp/ActivityRecoveryPassword.kt
@@ -0,0 +1,11 @@
+package com.example.taskapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityRecoveryPassword : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recovery_password)
+    }
+}
--- app/src/main/java/com/example/taskapp/MainActivity.kt
+++ app/src/main/java/com/example/taskapp/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.taskapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tvToRecoveryPassword = findViewById<TextView>(R.id.activity_login_tv_recovery_password)
+
+        tvToRecoveryPassword.setOnClickListener {
+            val intent = Intent(this, ActivityRecoveryPassword::class.java)
+            startActivity(intent)
+        }
     }
 }
